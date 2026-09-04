0e2b607882ba25f9c1e7d3a614e1f44ac64b33b5
archanarana-a <archana.rana@ongrid.in>
2026-09-04 11:37:49 +0530

    feat: add SonarQube scanning test class
    - Added test class to validate pipeline scanning
    - This code change will trigger SonarQube analysis

--- Projects/Brick-Breaker-Game/src/GameMain.java
+++ Projects/Brick-Breaker-Game/src/GameMain.java
@@ -1,1 +1,8 @@
 // test: Fri Sep  4 11:32:11 IST 2026
+
+// Feature: SonarQube scanning test
+public class ScanTest {
+    public static void main(String[] args) {
+        System.out.println("Testing SonarQube integration");
+    }
+}
